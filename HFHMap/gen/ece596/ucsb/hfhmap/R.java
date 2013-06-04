651ea1fd3acfe9a90c5da17c5a9cfc8691ab3781
Damein <Morgan>
2013-06-04 10:35:06 -0700

    update to HFH map

--- HFHMap/gen/ece596/ucsb/hfhmap/R.java
+++ HFHMap/gen/ece596/ucsb/hfhmap/R.java
@@ -187,15 +187,21 @@ containing a value of this type.
     }
     public static final class id {
         public static final int LinearLayout01=0x7f050006;
-        public static final int action_settings=0x7f05000d;
-        public static final int button1=0x7f050007;
-        public static final int button2=0x7f050009;
-        public static final int button3=0x7f05000b;
+        public static final int action_settings=0x7f050013;
+        public static final int angle_WRTN=0x7f050011;
+        public static final int angle_btn=0x7f050009;
+        public static final int edit_name=0x7f05000e;
+        public static final int enter_name=0x7f050010;
+        public static final int enter_step_len=0x7f050012;
         public static final int hybrid=0x7f050004;
+        public static final int lbl_your_name=0x7f05000f;
         public static final int map=0x7f050005;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
+        public static final int reset=0x7f05000d;
         public static final int satellite=0x7f050002;
+        public static final int step_btn=0x7f05000b;
+        public static final int step_len_btn=0x7f050007;
         public static final int terrain=0x7f050003;
         public static final int textview1=0x7f050008;
         public static final int textview2=0x7f05000a;
@@ -203,6 +209,8 @@ containing a value of this type.
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_enter_angle=0x7f030001;
+        public static final int step_len=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f090000;
@@ -275,6 +283,7 @@ containing a value of this type.
         /**  Long form sign-in button text [CHAR LIMIT=30] 
          */
         public static final int common_signin_button_text_long=0x7f06000e;
+        public static final int done_str=0x7f06001a;
         /**  Location client code resources (prefix with location_client) 
 
         Begin strings for location.reporting
